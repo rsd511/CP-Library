7b38863b8a533ec8bb7479a3e77e23f623f1db8d
Rahul <rahul.rsd511@gmail.com>
2019-11-18 22:44:21 +0530

    Updated UFDS to OOP Style

--- Data Structures/ufds.cpp
+++ Data Structures/ufds.cpp
@@ -1,30 +1,33 @@
 // OUTSIDE MAIN
 
 
-vector <ll> parent, Rank;
+// UFDS object_name(n);
+class UFDS {
+public:
 
-// For 0 to n-1
-void constructUFDS(ll n)
-{
-	parent.clear(); Rank.clear();
-	f(i,0,n-1) parent.pb(i), Rank.pb(0);
-}
+	vector <ll> parent, Rank;
 
-ll findSet(ll u)
-{
-	if(parent[u] == u) return u;
-	else return parent[u] = findSet(parent[u]);
-}
-
-ll isSameSet(ll u, ll v) { return (findSet(u) == findSet(v)); }
-
-void unionSet(ll u, ll v)
-{
-	if(isSameSet(u,v)) return;
-	ll pu = findSet(u), pv = findSet(v);
-	if(Rank[pu] > Rank[pv]) parent[pv] = pu;
-	else {
-		parent[pu] = pv;
-		if(Rank[pu] == Rank[pv]) Rank[pv]++;	
+	// For 0 to n-1
+	UFDS(ll n) {
+		parent.clear(); Rank.clear();
+		f(i,0,n-1) parent.pb(i), Rank.pb(0);
 	}
-}
+
+	ll findSet(ll u) {
+		if(parent[u] == u) return u;
+		else return parent[u] = findSet(parent[u]);
+	}
+
+	ll isSameSet(ll u, ll v) { return (findSet(u) == findSet(v)); }
+
+	void unionSet(ll u, ll v) {
+		if(isSameSet(u,v)) return;
+		ll pu = findSet(u), pv = findSet(v);
+		if(Rank[pu] > Rank[pv]) parent[pv] = pu;
+		else {
+			parent[pu] = pv;
+			if(Rank[pu] == Rank[pv]) Rank[pv]++;	
+		}
+	}
+
+};
