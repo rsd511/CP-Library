b5dc61c09efcf511343d0275d316c4e66f725abc
Rahul Sawantdesai <rahul.sawant17@siesgst.ac.in>
2020-06-07 19:02:01 +0530

    Added Compute_Hash function

---
+++ String/hashComputer.cpp
@@ -0,0 +1,11 @@
+ll compute_hash(string& s) {
+    ll p = 31;
+    ll m = 1e9 + 9;
+    ll hash_value = 0;
+    ll p_pow = 1;
+    fa(c,s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
